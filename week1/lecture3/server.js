b34686603ef6c184e1a709b9a8d56bfc7e817a6f
anshkataria16 <katariaansh367@gmail.com>
2026-08-11 10:57:30 +0530

    first commit

---
+++ week1/lecture3/server.js
@@ -0,0 +1,13 @@
+// const { log } = require('console')
+// const os = require('os')
+import { add,subtract } from './math.js'
+// console.log(os.platform())
+// console.log(os.arch())
+// console.log(os.cpus())
+// console.log(os.freemem()/(1024*1024*1024));
+
+console.log(add(2,3));
+console.log(subtract(2,3));
+
+
+// console.log("hello from lecture 3");
